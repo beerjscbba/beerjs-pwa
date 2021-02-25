0c454779436394e5a055eee276237be6e8346f89
frukus <frukolokoto@gmail.com>
2021-02-25 00:25:01 -0400

    base

---
+++ pages/inspire.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <img
+        src="/v.png"
+        alt="Vuetify.js"
+        class="mb-5"
+      >
+      <blockquote class="blockquote">
+        &#8220;First, solve the problem. Then, write the code.&#8221;
+        <v-btn
+          class="mx-2"
+          fab
+          dark
+          large
+          color="cyan"
+        >
+          <v-icon dark>
+            mdi-pencil
+          </v-icon>
+        </v-btn>
+        <v-btn
+          class="mx-2"
+          fab
+          dark
+          color="teal"
+        >
+          <v-icon dark>
+            mdi-format-list-bulleted-square
+          </v-icon>
+        </v-btn>
+        <footer>
+          <small>
+            <em>&mdash;John Johnson</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-col>
+  </v-row>
+</template>
